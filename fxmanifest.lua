2b8f45143166c6bcfdb4dcdccd75d6f6217cbf1e
GhostForge <ghostforge2026@gmail.com>
2026-06-08 00:29:06 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'B2D2 | GhostForge'
 description 'FiveM Serverstatus-Sender für externe Status-Integrationen mit dem Script gf_loadingscreen-V1 und einem Discord Bot'
-version '2.0.0'
+version '1.0.0'
 
 server_scripts {
     'config.lua',
